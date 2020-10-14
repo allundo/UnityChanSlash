07503b73af57109af9a283824fef145eb5787111
Allundo <chickenbomber@gmail.com>
2020-10-14 18:16:53 +0900

    Replace Cinemachine with Unity default Camera

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public Transform lookAt;
+    public Vector3 followOffset = default;
+    public Vector3 cameraPosition = default;
+
+    private Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = lookAt.position + lookAt.rotation * cameraPosition;
+
+        transform.LookAt(lookAt.position + lookAt.rotation * followOffset);
+    }
+}
