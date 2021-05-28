39ade031d66f06ef1fb2a7935cd3e6165b10eb8d
Allundo <chickenbomber@gmail.com>
2021-05-28 20:18:22 +0900

    Shortcut component finding method

--- Assets/Scripts/PlayerStatus.cs
+++ Assets/Scripts/PlayerStatus.cs
@@ -7,22 +7,23 @@ public class PlayerStatus : MobStatus
 {
 
     protected PlayerCommander playerCommander = default;
-    protected UnityChanAnimeHandler animeHandler = default;
+    protected UnityChanAnimeHandler playerAnim = default;
 
     protected override float Shield(Direction attackDir) => playerCommander.IsShieldOn(attackDir) ? 1 : 0;
 
     protected override void Start()
     {
         base.Start();
-        animeHandler = GetComponent<UnityChanAnimeHandler>();
-        playerCommander = GetComponent<PlayerCommander>();
+
+        playerCommander = commander as PlayerCommander;
+        playerAnim = playerCommander.anim as UnityChanAnimeHandler;
     }
 
     protected override void OnDamage(float damage, float shield)
     {
         if (shield > 0)
         {
-            animeHandler.shield.Fire();
+            playerAnim.shield.Fire();
         }
 
         base.OnDamage(damage, shield);
