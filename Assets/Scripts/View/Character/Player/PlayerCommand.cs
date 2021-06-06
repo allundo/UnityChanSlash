053ea200c1ae110c97371d79e6ccf311b2c5dcd0
Allundo <chickenbomber@gmail.com>
2021-06-06 23:50:49 +0900

    Set reserved attack events as playingTween to be cancelable

--- Assets/Scripts/View/Character/Player/PlayerCommand.cs
+++ Assets/Scripts/View/Character/Player/PlayerCommand.cs
@@ -231,7 +231,7 @@ public partial class PlayerCommander : ShieldCommander
         protected override void Action()
         {
             anim.attack.Fire();
-            jab.SetAttack(duration);
+            playingTween = jab.SetAttack(duration);
         }
     }
 
@@ -242,7 +242,7 @@ public partial class PlayerCommander : ShieldCommander
         protected override void Action()
         {
             anim.straight.Fire();
-            straight.SetAttack(duration);
+            playingTween = straight.SetAttack(duration);
         }
     }
     protected class PlayerKick : PlayerAttack
@@ -252,7 +252,7 @@ public partial class PlayerCommander : ShieldCommander
         protected override void Action()
         {
             anim.kick.Fire();
-            kick.SetAttack(duration);
+            playingTween = kick.SetAttack(duration);
         }
     }
 
