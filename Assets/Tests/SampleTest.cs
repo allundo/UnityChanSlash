50f12e4f8cd8ca5469ea76793890fae0b6010d2c
Allundo <chickenbomber@gmail.com>
2021-06-08 15:40:09 +0900

    Introduce Test Runner

---
+++ Assets/Tests/SampleTest.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+public class Example
+{
+    // Testアトリビュートを付ける
+    [Test]
+    public void ExampleTest()
+    {
+        // 条件式がtrueだったら成功
+        Assert.That(true);
+    }
+}
