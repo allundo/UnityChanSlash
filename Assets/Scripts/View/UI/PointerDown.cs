067a99ef1412e3d0b145c44e13fadf8c57f72b82
Allundo <chickenbomber@gmail.com>
2021-06-17 14:05:50 +0900

    Fix: shorten setting alpha duration of PointerDown button

--- Assets/Scripts/View/UI/PointerDown.cs
+++ Assets/Scripts/View/UI/PointerDown.cs
@@ -20,6 +20,6 @@ public class PointerDown : MoveButton, IPointerDownHandler
         Resize(1.5f);
         SetAlpha(1.0f);
         shrink = GetResize(1.0f, 0.4f).Play();
-        defaultAlpha = GetToAlpha(0.4f, 4.0f).Play();
+        defaultAlpha = GetToAlpha(0.4f, 0.4f).Play();
     }
 }
