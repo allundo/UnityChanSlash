025f6d59eba5dbc3cd9b1f2ba60863e8caedb6e4
Allundo <chickenbomber@gmail.com>
2021-06-24 19:16:13 +0900

    Implement Title logo animation with loading main scene

---
+++ Assets/Scripts/View/Title/StartButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class StartButton : MonoBehaviour
+{
+    private RectTransform rt;
+    private Image image;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rt = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+        rt.anchoredPosition = new Vector2(0f, 1920.0f);
+    }
+
+    // Update is called once per frame
+    void Start()
+    {
+
+
+    }
+}
