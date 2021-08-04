24bc484d490739f1fb35e71741df80e3998a146f
Allundo <chickenbomber@gmail.com>
2021-08-04 20:36:55 +0900

    Split responsibility of updating HidePlate into inner class

--- Assets/Scripts/View/Character/Player/HidePool.cs
+++ Assets/Scripts/View/Character/Player/HidePool.cs
@@ -14,7 +14,16 @@ public class HidePool : MonoBehaviour
     protected GameObject[] prefab = new GameObject[0b10000];
     protected Quaternion[] rotate = new Quaternion[0b10000];
 
+    protected LandscapeUpdater landscape = null;
+    protected HidePlateUpdater portrateN = null;
+    protected HidePlateUpdater portrateE = null;
+    protected HidePlateUpdater portrateS = null;
+    protected HidePlateUpdater portrateW = null;
+
+    private HidePlateUpdater currentUpdater = null;
+
     private WorldMap map;
+    private MapUtil mapUtil;
 
     protected const int RANGE = 11;
     protected HidePlate[,] plateData;
@@ -25,13 +34,15 @@ public class HidePool : MonoBehaviour
     void Awake()
     {
         map = GameManager.Instance.worldMap;
+        mapUtil = GetComponent<MapUtil>();
         InitHidePlates();
+
+        landscape = new LandscapeUpdater(this, RANGE);
+        currentUpdater = landscape;
     }
 
     private void InitHidePlates()
     {
-        this.plateData = new HidePlate[RANGE + map.Width, RANGE + map.Height];
-
         pool[(int)Plate.A] = pool[(int)Plate.B] = pool[(int)Plate.D] = pool[(int)Plate.C] = new GameObject("Plate1").transform;
         pool[(int)Plate.AB] = pool[(int)Plate.BD] = pool[(int)Plate.CD] = pool[(int)Plate.AC] = new GameObject("Plate2").transform;
         pool[(int)Plate.ABD] = pool[(int)Plate.BCD] = pool[(int)Plate.ACD] = pool[(int)Plate.ABC] = new GameObject("Plate3").transform;
@@ -76,7 +87,7 @@ public class HidePool : MonoBehaviour
     private void UpdateRange(Action<Pos, Plate[,]> DrawAction)
     {
         Pos playerPos = CurrentPos;
-        Plate[,] plateMap = GetPlateMap(playerPos);
+        Plate[,] plateMap = currentUpdater.GetPlateMap(playerPos);
 
         DrawAction(playerPos, plateMap);
 
@@ -85,231 +96,265 @@ public class HidePool : MonoBehaviour
 
     public void Init()
     {
-        UpdateRange((playerPos, plateMap) => InitRange(playerPos, plateMap));
+        UpdateRange((playerPos, plateMap) => currentUpdater.InitRange(playerPos, plateMap));
     }
 
     public void Redraw()
     {
-        UpdateRange((playerPos, plateMap) => RedrawRange(playerPos, plateMap));
+        UpdateRange((playerPos, plateMap) => currentUpdater.RedrawRange(playerPos, plateMap));
     }
 
     public void Move()
     {
-        UpdateRange((playerPos, plateMap) => MoveRange(playerPos, plateMap));
+        UpdateRange((playerPos, plateMap) => currentUpdater.MoveRange(playerPos, plateMap));
     }
 
-    private void InitRange(Pos playerPos, Plate[,] plateMap)
+    protected abstract class HidePlateUpdater
     {
-        for (int j = 0; j < RANGE; j++)
+        protected HidePool hidePool;
+        protected HidePlate GetInstance(Plate plate, Pos pos, float duration = 0.01f) => hidePool.GetInstance(plate, pos, duration);
+        protected WorldMap map => hidePool.map;
+        protected Pos prevPos => hidePool.prevPos;
+        protected HidePlate[,] plateData => hidePool.plateData;
+
+        protected int width;
+        protected int height;
+
+        protected HidePlateUpdater(HidePool hidePool, int width, int height)
         {
-            for (int i = 0; i < RANGE; i++)
-            {
-                int x = playerPos.x + i;
-                int y = playerPos.y + j;
-                plateData[x, y] = GetInstance(plateMap[i, j], OffsetPos(playerPos, i, j));
-            }
+            this.hidePool = hidePool;
+
+            int maxRange = 2 * Mathf.Max(width, height) - Mathf.Min(width, height) - 1;
+            hidePool.plateData = new HidePlate[map.Width + maxRange, map.Height + maxRange];
+
+            this.width = width;
+            this.height = height;
         }
-    }
 
-    private void RedrawRange(Pos playerPos, Plate[,] plateMap, int xShrink = 0, int yShrink = 0)
-    {
-        for (int j = yShrink; j < RANGE - yShrink; j++)
+        /// <summary>
+        /// Light weight simple version of RedrawRange() method
+        /// </summary>
+        public void InitRange(Pos playerPos, Plate[,] plateMap)
         {
-            for (int i = xShrink; i < RANGE - xShrink; i++)
+            for (int j = 0; j < height; j++)
             {
-                int x = playerPos.x + i;
-                int y = playerPos.y + j;
-
-                if (plateMap[i, j] != (plateData[x, y] == null ? Plate.NONE : plateData[x, y].plate))
+                for (int i = 0; i < width; i++)
                 {
-                    plateData[x, y]?.Remove(0.25f);
-                    plateData[x, y] = GetInstance(plateMap[i, j], OffsetPos(playerPos, i, j), 0.25f);
+                    int x = playerPos.x + i;
+                    int y = playerPos.y + j;
+                    plateData[x, y] = GetInstance(plateMap[i, j], OffsetPos(playerPos, i, j));
                 }
             }
         }
-    }
-    private void RedrawXShrink(Pos playerPos, Plate[,] plateMap) => RedrawRange(playerPos, plateMap, 1, 0);
-    private void RedrawYShrink(Pos playerPos, Plate[,] plateMap) => RedrawRange(playerPos, plateMap, 0, 1);
 
-    private void MoveRange(Pos playerPos, Plate[,] plateMap)
+        public void RedrawRange(Pos playerPos, Plate[,] plateMap, int xShrink = 0, int yShrink = 0)
+        {
+            for (int j = yShrink; j < height - yShrink; j++)
+            {
+                for (int i = xShrink; i < width - xShrink; i++)
+                {
+                    int x = playerPos.x + i;
+                    int y = playerPos.y + j;
+
+                    if (plateMap[i, j] != (plateData[x, y] == null ? Plate.NONE : plateData[x, y].plate))
+                    {
+                        plateData[x, y]?.Remove(0.25f);
+                        plateData[x, y] = GetInstance(plateMap[i, j], OffsetPos(playerPos, i, j), 0.25f);
+                    }
+                }
+            }
+        }
+        private void RedrawXShrink(Pos playerPos, Plate[,] plateMap) => RedrawRange(playerPos, plateMap, 1, 0);
+        private void RedrawYShrink(Pos playerPos, Plate[,] plateMap) => RedrawRange(playerPos, plateMap, 0, 1);
+
+        public void MoveRange(Pos playerPos, Plate[,] plateMap)
+        {
+            Pos moveVec = playerPos - prevPos;
+
+            if (moveVec.y < 0) MoveRangeNorth(playerPos, plateMap);
+            if (moveVec.y > 0) MoveRangeSouth(playerPos, plateMap);
+            if (moveVec.x < 0) MoveRangeWest(playerPos, plateMap);
+            if (moveVec.x > 0) MoveRangeEast(playerPos, plateMap);
+        }
+
+        private void MoveRangeNorth(Pos playerPos, Plate[,] plateMap)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                plateData[playerPos.x + i, playerPos.y + height]?.Remove();
+                plateData[playerPos.x + i, playerPos.y] = GetInstance(plateMap[i, 0], OffsetPos(playerPos, i, 0));
+            }
+            RedrawYShrink(playerPos, plateMap);
+        }
+        private void MoveRangeSouth(Pos playerPos, Plate[,] plateMap)
+        {
+            for (int i = 0; i < width; i++)
+            {
+                plateData[playerPos.x + i, playerPos.y - 1]?.Remove();
+                plateData[playerPos.x + i, playerPos.y + height - 1] = GetInstance(plateMap[i, height - 1], OffsetPos(playerPos, i, height - 1));
+            }
+            RedrawYShrink(playerPos, plateMap);
+        }
+        private void MoveRangeWest(Pos playerPos, Plate[,] plateMap)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                plateData[playerPos.x + width, playerPos.y + j]?.Remove();
+                plateData[playerPos.x, playerPos.y + j] = GetInstance(plateMap[0, j], OffsetPos(playerPos, 0, j));
+            }
+            RedrawXShrink(playerPos, plateMap);
+        }
+        private void MoveRangeEast(Pos playerPos, Plate[,] plateMap)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                plateData[playerPos.x - 1, playerPos.y + j]?.Remove();
+                plateData[playerPos.x + width - 1, playerPos.y + j] = GetInstance(plateMap[width - 1, j], OffsetPos(playerPos, width - 1, j));
+            }
+            RedrawXShrink(playerPos, plateMap);
+        }
+
+        private Pos OffsetPos(Pos pos, int offsetX, int offsetY)
+            => new Pos(pos.x - width / 2 + offsetX, pos.y - height / 2 + offsetY);
+
+        /// <summary>
+        /// Get tiles inside player view range with see through info
+        /// </summary>
+        private bool[,] GetTileRegion(int x, int y)
+        {
+            int tileWidth = width + 2;
+            int tileHeight = height + 2;
+
+            var region = new bool[tileWidth, tileHeight];
+
+            for (int i = 0; i < tileWidth; i++)
+            {
+                region[i, 0] = region[i, tileHeight - 1] = false;
+            }
+
+            for (int j = 1; j < tileHeight - 1; j++)
+            {
+                region[0, j] = region[tileWidth - 1, j] = false;
+            }
+
+            for (int j = 1; j < tileHeight - 1; j++)
+            {
+                for (int i = 1; i < tileWidth - 1; i++)
+                {
+                    int matX = x - tileWidth / 2 + i;
+                    int matY = y - tileHeight / 2 + j;
+
+                    region[i, j] = map.IsTileViewOpen(matX, matY);
+                }
+            }
+
+            return region;
+        }
+
+        /// <summary>
+        /// Hide plate placing data
+        /// </summary>
+        /// <param name="playerPos"></param>
+        /// <returns></returns>
+        public Plate[,] GetPlateMap(Pos playerPos)
+        {
+            // Get 13x13 player view range
+            bool[,] region = GetTileRegion(playerPos.x, playerPos.y);
+
+            var plateMap = new Plate[width, height];
+
+            // Fill 11x11 tile with hide plates nearby player
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    plateMap[i, j] = Plate.ABCD;
+                }
+            }
+
+            var openStack = new Stack<(int x, int y)>();
+
+            // Start from player tile
+            for (openStack.Push((width / 2, height / 2)); openStack.Count > 0;)
+            {
+                var pos = openStack.Pop();
+
+                // Delete focused hide plate
+                plateMap[pos.x, pos.y] = Plate.NONE;
+
+                // plateMap index[x, y] = region index[x+1, y+1]
+                int ptX = pos.x + 1;
+                int ptY = pos.y + 1;
+
+                region[ptX, ptY] = false;
+
+                // Delete corner of hide plates nearby focusing tile
+                if (pos.x - 1 >= 0 && pos.y - 1 >= 0)
+                {
+                    plateMap[pos.x - 1, pos.y - 1] &= ~Plate.D;
+                }
+
+                if (pos.x - 1 >= 0 && pos.y + 1 < height)
+                {
+                    plateMap[pos.x - 1, pos.y + 1] &= ~Plate.B;
+                }
+
+                if (pos.x + 1 < width && pos.y - 1 >= 0)
+                {
+                    plateMap[pos.x + 1, pos.y - 1] &= ~Plate.C;
+                }
+
+                if (pos.x + 1 < width && pos.y + 1 < height)
+                {
+                    plateMap[pos.x + 1, pos.y + 1] &= ~Plate.A;
+                }
+
+                // Push neighbor tile as hide plate deleting candidate
+                // If not a delete candidate, delete only a half of the plate
+                if (region[ptX - 1, ptY])
+                {
+                    openStack.Push((pos.x - 1, pos.y));
+                }
+                else if (pos.x - 1 >= 0)
+                {
+                    plateMap[pos.x - 1, pos.y] &= ~Plate.BD;
+                }
+
+                if (region[ptX, ptY - 1])
+                {
+                    openStack.Push((pos.x, pos.y - 1));
+                }
+                else if (pos.y - 1 >= 0)
+                {
+                    plateMap[pos.x, pos.y - 1] &= ~Plate.CD;
+                }
+
+                if (region[ptX + 1, ptY])
+                {
+                    openStack.Push((pos.x + 1, pos.y));
+                }
+                else if (pos.x + 1 < width)
+                {
+                    plateMap[pos.x + 1, pos.y] &= ~Plate.AC;
+                }
+
+                if (region[ptX, ptY + 1])
+                {
+                    openStack.Push((pos.x, pos.y + 1));
+                }
+                else if (pos.y + 1 < height)
+                {
+                    plateMap[pos.x, pos.y + 1] &= ~Plate.AB;
+                }
+            }
+
+            return plateMap;
+        }
+    }
+    protected class LandscapeUpdater : HidePlateUpdater
     {
-        Pos moveVec = playerPos - prevPos;
-
-        if (moveVec.y < 0) MoveRangeNorth(playerPos, plateMap);
-        if (moveVec.y > 0) MoveRangeSouth(playerPos, plateMap);
-        if (moveVec.x < 0) MoveRangeWest(playerPos, plateMap);
-        if (moveVec.x > 0) MoveRangeEast(playerPos, plateMap);
+        public LandscapeUpdater(HidePool hidePool, int range) : base(hidePool, range, range)
+        { }
     }
 
-    private void MoveRangeNorth(Pos playerPos, Plate[,] plateMap)
-    {
-        for (int i = 0; i < RANGE; i++)
-        {
-            plateData[playerPos.x + i, playerPos.y + RANGE]?.Remove();
-            plateData[playerPos.x + i, playerPos.y] = GetInstance(plateMap[i, 0], OffsetPos(playerPos, i, 0));
-        }
-        RedrawYShrink(playerPos, plateMap);
-    }
-    private void MoveRangeSouth(Pos playerPos, Plate[,] plateMap)
-    {
-        for (int i = 0; i < RANGE; i++)
-        {
-            plateData[playerPos.x + i, playerPos.y - 1]?.Remove();
-            plateData[playerPos.x + i, playerPos.y + RANGE - 1] = GetInstance(plateMap[i, RANGE - 1], OffsetPos(playerPos, i, RANGE - 1));
-        }
-        RedrawYShrink(playerPos, plateMap);
-    }
-    private void MoveRangeWest(Pos playerPos, Plate[,] plateMap)
-    {
-        for (int j = 0; j < RANGE; j++)
-        {
-            plateData[playerPos.x + RANGE, playerPos.y + j]?.Remove();
-            plateData[playerPos.x, playerPos.y + j] = GetInstance(plateMap[0, j], OffsetPos(playerPos, 0, j));
-        }
-        RedrawXShrink(playerPos, plateMap);
-    }
-    private void MoveRangeEast(Pos playerPos, Plate[,] plateMap)
-    {
-        for (int j = 0; j < RANGE; j++)
-        {
-            plateData[playerPos.x - 1, playerPos.y + j]?.Remove();
-            plateData[playerPos.x + RANGE - 1, playerPos.y + j] = GetInstance(plateMap[RANGE - 1, j], OffsetPos(playerPos, RANGE - 1, j));
-        }
-        RedrawXShrink(playerPos, plateMap);
-    }
-
-    private Pos OffsetPos(Pos pos, int offsetX, int offsetY)
-        => new Pos(pos.x - RANGE / 2 + offsetX, pos.y - RANGE / 2 + offsetY);
-
-    /// <summary>
-    /// Get tiles inside player view range with see through info
-    /// </summary>
-    private bool[,] GetTileRegion(int x, int y)
-    {
-        int tileRange = RANGE + 2;
-        var region = new bool[tileRange, tileRange];
-
-        for (int i = 0; i < tileRange; i++)
-        {
-            region[i, 0] = region[i, tileRange - 1] = false;
-        }
-
-        for (int j = 1; j < tileRange - 1; j++)
-        {
-            region[0, j] = region[tileRange - 1, j] = false;
-        }
-
-        for (int j = 1; j < tileRange - 1; j++)
-        {
-            for (int i = 1; i < tileRange - 1; i++)
-            {
-                int matX = x - tileRange / 2 + i;
-                int matY = y - tileRange / 2 + j;
-
-                region[i, j] = map.IsTileViewOpen(matX, matY);
-            }
-        }
-
-        return region;
-    }
-
-    /// <summary>
-    /// Hide plate placing data
-    /// </summary>
-    /// <param name="playerPos"></param>
-    /// <returns></returns>
-    private Plate[,] GetPlateMap(Pos playerPos)
-    {
-        // Get 13x13 player view range
-        bool[,] region = GetTileRegion(playerPos.x, playerPos.y);
-
-        var plateMap = new Plate[RANGE, RANGE];
-
-        // Fill 11x11 tile with hide plates nearby player
-        for (int j = 0; j < RANGE; j++)
-        {
-            for (int i = 0; i < RANGE; i++)
-            {
-                plateMap[i, j] = Plate.ABCD;
-            }
-        }
-
-        var openStack = new Stack<(int x, int y)>();
-
-        // Start from player tile
-        for (openStack.Push((RANGE / 2, RANGE / 2)); openStack.Count > 0;)
-        {
-            var pos = openStack.Pop();
-
-            // Delete focused hide plate
-            plateMap[pos.x, pos.y] = Plate.NONE;
-
-            // plateMap index[x, y] = region index[x+1, y+1]
-            int ptX = pos.x + 1;
-            int ptY = pos.y + 1;
-
-            region[ptX, ptY] = false;
-
-            // Delete corner of hide plates nearby focusing tile
-            if (pos.x - 1 >= 0 && pos.y - 1 >= 0)
-            {
-                plateMap[pos.x - 1, pos.y - 1] &= ~Plate.D;
-            }
-
-            if (pos.x - 1 >= 0 && pos.y + 1 < RANGE)
-            {
-                plateMap[pos.x - 1, pos.y + 1] &= ~Plate.B;
-            }
-
-            if (pos.x + 1 < RANGE && pos.y - 1 >= 0)
-            {
-                plateMap[pos.x + 1, pos.y - 1] &= ~Plate.C;
-            }
-
-            if (pos.x + 1 < RANGE && pos.y + 1 < RANGE)
-            {
-                plateMap[pos.x + 1, pos.y + 1] &= ~Plate.A;
-            }
-
-            // Push neighbor tile as hide plate deleting candidate
-            // If not a delete candidate, delete only a half of the plate
-            if (region[ptX - 1, ptY])
-            {
-                openStack.Push((pos.x - 1, pos.y));
-            }
-            else if (pos.x - 1 >= 0)
-            {
-                plateMap[pos.x - 1, pos.y] &= ~Plate.BD;
-            }
-
-            if (region[ptX, ptY - 1])
-            {
-                openStack.Push((pos.x, pos.y - 1));
-            }
-            else if (pos.y - 1 >= 0)
-            {
-                plateMap[pos.x, pos.y - 1] &= ~Plate.CD;
-            }
-
-            if (region[ptX + 1, ptY])
-            {
-                openStack.Push((pos.x + 1, pos.y));
-            }
-            else if (pos.x + 1 < RANGE)
-            {
-                plateMap[pos.x + 1, pos.y] &= ~Plate.AC;
-            }
-
-            if (region[ptX, ptY + 1])
-            {
-                openStack.Push((pos.x, pos.y + 1));
-            }
-            else if (pos.y + 1 < RANGE)
-            {
-                plateMap[pos.x, pos.y + 1] &= ~Plate.AB;
-            }
-        }
-
-        return plateMap;
-    }
 }
