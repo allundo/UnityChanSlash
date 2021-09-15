b7eb20238fb97944e6ea7a0096c91df1ea9a89ed
Allundo <chickenbomber@gmail.com>
2021-09-15 13:15:53 +0900

    Add new scene for testing

---
+++ Assets/Scripts/Presenter/TestSceneMediator.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+using System;
+
+public class TestSceneMediator : SceneMediator
+{
+    [SerializeField] private Button button = default;
+    [SerializeField] private CoverScreen cover = default;
+
+    protected override void InitBeforeStart()
+    {
+        SetStartActions(() => { });
+
+        button.onClick
+            .AsObservable()
+            .First(_ => true)
+            .ContinueWith(_ => LoadSceneWithCoverOn(2))
+            .IgnoreElements()
+            .Subscribe(null, () => SceneTransition(0))
+            .AddTo(this);
+    }
+
+    private IObservable<Unit> LoadSceneWithCoverOn(int sceneBuildIndex, float duration = 2f)
+        => Observable.Merge(
+            sceneLoader.LoadSceneAsync(sceneBuildIndex),
+            cover.CoverOn(duration).OnCompleteAsObservable().Select(t => Unit.Default)
+        );
+}
---
+++ Assets/Scripts/ReloadScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour
+{
+    void Start()
+    {
+        var button = GetComponent<Button>();
+        button.onClick.AddListener(Reload);
+    }
+
+    void Reload()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
