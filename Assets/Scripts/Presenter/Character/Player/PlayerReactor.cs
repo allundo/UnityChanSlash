49475b6df6fba8eb87c5e3f12de4bcc619966390
Allundo <chickenbomber@gmail.com>
2021-09-19 14:33:29 +0900

    Set UI invisible on player dead

--- Assets/Scripts/Presenter/Character/Player/PlayerReactor.cs
+++ Assets/Scripts/Presenter/Character/Player/PlayerReactor.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 
 [RequireComponent(typeof(GuardState))]
+[RequireComponent(typeof(PlayerCommander))]
 public class PlayerReactor : MobReactor
 {
     protected GuardState guardState;
+    protected PlayerCommander playerCommander;
 
     protected override void Awake()
     {
         base.Awake();
         guardState = GetComponent<GuardState>();
+        playerCommander = GetComponent<PlayerCommander>();
     }
 
     protected override float CalcDamage(float attack, IDirection dir)
@@ -23,5 +26,12 @@ public class PlayerReactor : MobReactor
 
         return Mathf.Max(status.CalcAttack(attack, dir) - shield, 0.0f);
     }
+
+    protected override void OnDie()
+    {
+        base.OnDie();
+        playerCommander.InvisibleInput();
+    }
+
     protected override void Dead() => Inactivate();
 }
