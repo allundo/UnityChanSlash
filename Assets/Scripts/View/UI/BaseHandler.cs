62f93789aeb1ef66d9fda79ea3b0997ffbb77aed
Allundo <chickenbomber@gmail.com>
2021-09-21 17:42:08 +0900

    Divide base responsibilities of handling UI into new class

---
+++ Assets/Scripts/View/UI/BaseHandler.cs
@@ -0,0 +1,236 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+using System;
+using UniRx;
+
+public class BaseHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    [SerializeField] protected HandleUI handleRUI = default;
+    [SerializeField] protected HandleUI handleLUI = default;
+    [SerializeField] protected FlickInteraction flickR = default;
+    [SerializeField] protected FlickInteraction flickL = default;
+    [SerializeField] protected float maxAlpha = 0.8f;
+
+    protected IObservable<Unit> ObserveUp => Observable.Merge(flickR.UpSubject, flickL.UpSubject);
+    protected IObservable<Unit> ObserveDown => Observable.Merge(flickR.DownSubject, flickL.DownSubject);
+    protected IObservable<Unit> ObserveRL => Observable.Merge(flickR.LeftSubject, flickL.RightSubject);
+
+    protected RawImage rawImage;
+
+    protected float alpha = 0.0f;
+    protected bool isActive = false;
+    private bool isFingerDown = false;
+
+    protected HandleUI[] handleUIs;
+    private FlickInteraction currentFlick = null;
+    private Vector2 pressPos = Vector2.zero;
+
+    public bool IsPressed => currentFlick != null;
+
+    private Vector2 DragVector(Vector2 screenPos) => screenPos - pressPos;
+
+    protected virtual void Awake()
+    {
+        rawImage = GetComponent<RawImage>();
+
+        handleUIs = new[] { handleRUI, handleLUI };
+    }
+
+    protected virtual void Start()
+    {
+        ResetCenterPos();
+
+        SetAlpha(0.0f);
+        gameObject.SetActive(false);
+        InactivateButtons();
+    }
+
+    protected virtual void Update()
+    {
+        UpdateTransparent();
+    }
+
+    private void UpdateTransparent()
+    {
+        alpha += (isActive ? 3f : -6f) * Time.deltaTime;
+
+        if (alpha > maxAlpha)
+        {
+            alpha = maxAlpha;
+            return;
+        }
+
+        if (alpha < 0.0f)
+        {
+            alpha = 0.0f;
+
+            gameObject.SetActive(false);
+            InactivateButtons();
+
+            return;
+        }
+
+        SetAlpha(alpha);
+    }
+
+    protected virtual void SetAlpha(float alpha)
+    {
+        handleRUI.SetAlpha(alpha);
+        handleLUI.SetAlpha(alpha);
+    }
+
+    public virtual void OnPointerUp(PointerEventData eventData)
+    {
+        if (!isFingerDown) return;
+
+        isFingerDown = false;
+
+        if (currentFlick == null)
+        {
+            RaycastEvent<IPointerUpHandler>(eventData, (handler, data) => handler.OnPointerUp(data as PointerEventData));
+            return;
+        }
+
+        if (!isActive) return;
+
+        if (currentFlick == flickL) handleRUI.Activate(alpha);
+        if (currentFlick == flickR) handleLUI.Activate(alpha);
+
+        currentFlick.Release(DragVector(eventData.position));
+        currentFlick = null;
+    }
+
+    public virtual void OnPointerDown(PointerEventData eventData)
+    {
+        isFingerDown = true;
+
+        FlickInteraction flick = GetFlick(eventData.position);
+
+        if (flick == null)
+        {
+            RaycastEvent<IPointerDownHandler>(eventData, (handler, data) => handler.OnPointerDown(data as PointerEventData));
+            return;
+        }
+
+        if (!isActive) return;
+
+        pressPos = eventData.position;
+        currentFlick = flick;
+
+        if (currentFlick == flickL) handleRUI.Inactivate();
+        if (currentFlick == flickR) handleLUI.Inactivate();
+
+        currentFlick.UpdateImage(DragVector(eventData.position));
+    }
+
+    public virtual void OnDrag(PointerEventData eventData)
+    {
+        if (!isFingerDown) return;
+
+        if (currentFlick == null)
+        {
+            RaycastEvent<IDragHandler>(eventData, (handler, data) => handler.OnDrag(data as PointerEventData));
+            return;
+        }
+
+        currentFlick.UpdateImage(DragVector(eventData.position));
+    }
+
+    public void ResetCenterPos()
+    {
+        handleUIs.ForEach(handleUI => handleUI.ResetCenterPos());
+    }
+
+    protected virtual FlickInteraction GetFlick(Vector2 screenPos)
+    {
+        if (handleRUI.InRegion(screenPos)) return flickR;
+        if (handleLUI.InRegion(screenPos)) return flickL;
+
+        return null;
+    }
+
+    public void Activate()
+    {
+        if (isActive) return;
+
+        isActive = true;
+        gameObject.SetActive(true);
+
+        ActivateButtons();
+    }
+
+    protected virtual void ActivateButtons()
+    {
+        alpha = 0.0f;
+
+        handleRUI.Activate(alpha);
+        handleLUI.Activate(alpha);
+    }
+
+    private void InactivateButtons()
+    {
+        handleUIs.ForEach(handleUI => handleUI.Inactivate());
+        currentFlick?.Inactivate();
+    }
+
+    public void Inactivate()
+    {
+        if (!isActive) return;
+
+        ButtonCancel(true);
+        isActive = false;
+    }
+
+    protected void ButtonCancel(bool isFadeOnly = false)
+    {
+        if (isFadeOnly)
+        {
+            currentFlick?.Inactivate();
+        }
+        else
+        {
+            currentFlick?.Cancel();
+        }
+
+        isFingerDown = false;
+        currentFlick = null;
+        pressPos = Vector2.zero;
+    }
+
+    public virtual void SetActive(bool value)
+    {
+        if (value)
+        {
+            Activate();
+        }
+        else
+        {
+            Inactivate();
+        }
+    }
+
+    protected void RaycastEvent<T>(PointerEventData eventData, ExecuteEvents.EventFunction<T> eventFunc) where T : IEventSystemHandler
+    {
+        var objectsHit = new List<RaycastResult>();
+
+        // Exclude this UI object from raycast target
+        rawImage.raycastTarget = false;
+
+        EventSystem.current.RaycastAll(eventData, objectsHit);
+
+        rawImage.raycastTarget = true;
+
+        foreach (var objectHit in objectsHit)
+        {
+            if (!ExecuteEvents.CanHandleEvent<T>(objectHit.gameObject))
+            {
+                continue;
+            }
+
+            ExecuteEvents.Execute<T>(objectHit.gameObject, eventData, eventFunc);
+            break;
+        }
+    }
+}
--- Assets/Scripts/View/UI/DoorHandler/DoorHandler.cs
+++ Assets/Scripts/View/UI/DoorHandler/DoorHandler.cs
@@ -1,91 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
-using System.Collections.Generic;
 using System;
 using UniRx;
 
-public class DoorHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+public class DoorHandler : BaseHandler
 {
     [SerializeField] private HandleUI openDoorUI = default;
-    [SerializeField] private HandleUI closeRDoorUI = default;
-    [SerializeField] private HandleUI closeLDoorUI = default;
     [SerializeField] private DoorFlick openFlick = default;
-    [SerializeField] private DoorFlick closeRFlick = default;
-    [SerializeField] private DoorFlick closeLFlick = default;
-    [SerializeField] private float maxAlpha = 0.8f;
 
-    public IObservable<Unit> ObserveGo => Observable.Merge(closeRFlick.UpSubject, closeLFlick.UpSubject);
-    public IObservable<Unit> ObserveHandle => Observable.Merge(openFlick.RightSubject, openFlick.LeftSubject, closeRFlick.LeftSubject, closeLFlick.RightSubject);
-    public IObservable<bool> ObserveHandOn => Observable.Merge(openFlick.IsHandOn, closeRFlick.IsHandOn, closeLFlick.IsHandOn);
+    private DoorFlick doorFlickR;
+    private DoorFlick doorFlickL;
 
-    private RectTransform rectTransform;
-    private RawImage rawImage;
+    public IObservable<Unit> ObserveGo => ObserveUp;
+    public IObservable<Unit> ObserveHandle => Observable.Merge(openFlick.RightSubject, openFlick.LeftSubject, ObserveRL);
+    public IObservable<bool> ObserveHandOn => Observable.Merge(openFlick.IsHandOn, doorFlickR.IsHandOn, doorFlickL.IsHandOn);
 
-    private float alpha = 0.0f;
-    private bool isActive = false;
     private bool isOpen = false;
-    private bool isFingerDown = false;
 
-    private HandleUI[] doorUIs;
-    private FlickInteraction currentFlick = null;
-    private Vector2 pressPos = Vector2.zero;
-
-    public bool IsPressed => currentFlick != null;
-
-    private Vector2 DragVector(Vector2 screenPos) => screenPos - pressPos;
-
-    void Awake()
+    protected override void Awake()
     {
-        rectTransform = GetComponent<RectTransform>();
         rawImage = GetComponent<RawImage>();
+        handleUIs = new[] { openDoorUI, handleRUI, handleLUI };
 
-        doorUIs = new[] { openDoorUI, closeRDoorUI, closeLDoorUI };
+        doorFlickR = flickR as DoorFlick;
+        doorFlickL = flickL as DoorFlick;
     }
 
-    void Start()
-    {
-        ResetCenterPos();
-
-        SetAlpha(0.0f);
-        gameObject.SetActive(false);
-        InactivateButtons();
-    }
-
-    void Update()
-    {
-        UpdateTransparent();
-    }
-
-    private void UpdateTransparent()
-    {
-        alpha += (isActive ? 3f : -6f) * Time.deltaTime;
-
-        if (alpha > maxAlpha)
-        {
-            alpha = maxAlpha;
-            return;
-        }
-
-        if (alpha < 0.0f)
-        {
-            alpha = 0.0f;
-
-            gameObject.SetActive(false);
-            InactivateButtons();
-
-            return;
-        }
-
-        SetAlpha(alpha);
-    }
-
-    private void SetAlpha(float alpha)
+    protected override void SetAlpha(float alpha)
     {
         if (isOpen)
         {
-            closeRDoorUI.SetAlpha(alpha);
-            closeLDoorUI.SetAlpha(alpha);
+            base.SetAlpha(alpha);
         }
         else
         {
@@ -93,74 +38,11 @@ public class DoorHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         }
     }
 
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        if (!isFingerDown) return;
-
-        isFingerDown = false;
-
-        if (currentFlick == null)
-        {
-            RaycastEvent<IPointerUpHandler>(eventData, (handler, data) => handler.OnPointerUp(data as PointerEventData));
-            return;
-        }
-
-        if (!isActive) return;
-
-        if (currentFlick == closeLFlick) closeRDoorUI.Activate(alpha);
-        if (currentFlick == closeRFlick) closeLDoorUI.Activate(alpha);
-
-        currentFlick.Release(DragVector(eventData.position));
-        currentFlick = null;
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        isFingerDown = true;
-
-        FlickInteraction flick = GetFlick(eventData.position);
-
-        if (flick == null)
-        {
-            RaycastEvent<IPointerDownHandler>(eventData, (handler, data) => handler.OnPointerDown(data as PointerEventData));
-            return;
-        }
-
-        if (!isActive) return;
-
-        pressPos = eventData.position;
-        currentFlick = flick;
-
-        if (currentFlick == closeLFlick) closeRDoorUI.Inactivate();
-        if (currentFlick == closeRFlick) closeLDoorUI.Inactivate();
-
-        currentFlick.UpdateImage(DragVector(eventData.position));
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (!isFingerDown) return;
-
-        if (currentFlick == null)
-        {
-            RaycastEvent<IDragHandler>(eventData, (handler, data) => handler.OnDrag(data as PointerEventData));
-            return;
-        }
-
-        currentFlick.UpdateImage(DragVector(eventData.position));
-    }
-
-    public void ResetCenterPos()
-    {
-        doorUIs.ForEach(doorUI => doorUI.ResetCenterPos());
-    }
-
-    private FlickInteraction GetFlick(Vector2 screenPos)
+    protected override FlickInteraction GetFlick(Vector2 screenPos)
     {
         if (isOpen)
         {
-            if (closeRDoorUI.InRegion(screenPos)) return closeRFlick;
-            if (closeLDoorUI.InRegion(screenPos)) return closeLFlick;
+            return base.GetFlick(screenPos);
         }
         else
         {
@@ -181,52 +63,22 @@ public class DoorHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         ActivateButtons();
     }
 
-    private void ActivateButtons()
+    protected override void ActivateButtons()
     {
         alpha = 0.0f;
 
         if (isOpen)
         {
             openDoorUI.Inactivate();
-            closeRDoorUI.Activate(alpha);
-            closeLDoorUI.Activate(alpha);
+            handleRUI.Activate(alpha);
+            handleLUI.Activate(alpha);
         }
         else
         {
             openDoorUI.Activate(alpha);
-            closeRDoorUI.Inactivate();
-            closeLDoorUI.Inactivate();
+            handleRUI.Inactivate();
+            handleLUI.Inactivate();
         }
-    }
-
-    private void InactivateButtons()
-    {
-        doorUIs.ForEach(doorUI => doorUI.Inactivate());
-        currentFlick?.Inactivate();
-    }
-
-    public void Inactivate()
-    {
-        if (!isActive) return;
-
-        ButtonCancel(true);
-        isActive = false;
-    }
-
-    private void ButtonCancel(bool isFadeOnly = false)
-    {
-        if (isFadeOnly)
-        {
-            currentFlick?.Inactivate();
-        }
-        else
-        {
-            currentFlick?.Cancel();
-        }
-
-        isFingerDown = false;
-        currentFlick = null;
-        pressPos = Vector2.zero;
     }
 
     public void SetActive(bool value, bool isOpen)
@@ -238,29 +90,6 @@ public class DoorHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
         else
         {
             Inactivate();
-        }
-    }
-
-    private void RaycastEvent<T>(PointerEventData eventData, ExecuteEvents.EventFunction<T> eventFunc) where T : IEventSystemHandler
-    {
-        var objectsHit = new List<RaycastResult>();
-
-        // Exclude this UI object from raycast target
-        rawImage.raycastTarget = false;
-
-        EventSystem.current.RaycastAll(eventData, objectsHit);
-
-        rawImage.raycastTarget = true;
-
-        foreach (var objectHit in objectsHit)
-        {
-            if (!ExecuteEvents.CanHandleEvent<T>(objectHit.gameObject))
-            {
-                continue;
-            }
-
-            ExecuteEvents.Execute<T>(objectHit.gameObject, eventData, eventFunc);
-            break;
         }
     }
 }
