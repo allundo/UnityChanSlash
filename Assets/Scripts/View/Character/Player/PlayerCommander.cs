e661ee589e508ff15dc627e6f0e2a378b6405478
Allundo <chickenbomber@gmail.com>
2021-12-11 13:16:08 +0900

    Add description for Attack command canceling

--- Assets/Scripts/View/Character/Player/PlayerCommander.cs
+++ Assets/Scripts/View/Character/Player/PlayerCommander.cs
@@ -13,14 +13,19 @@ public class PlayerCommander : ShieldCommander
     public override void EnqueueCommand(Command cmd)
     {
         base.EnqueueCommand(cmd);
-        if (anim.cancel.Bool) CheckCancel();
+        if (anim.cancel.Bool) CheckCancel(); // Cancel current cancelable Attack if newly enqueued command is Attack
     }
 
     protected override void Subscribe(IObservable<Unit> execObservable)
     {
         if (execObservable == null) return;
 
-        execDisposable = execObservable.Subscribe(_ => CheckCancel(), () => DispatchCommand()).AddTo(targetObject);
+        execDisposable = execObservable
+            .Subscribe(
+                _ => CheckCancel(), // Cancel current Attack if next is also Attack on cancelation timing
+                () => DispatchCommand()
+            )
+            .AddTo(targetObject);
     }
 
     protected void CheckCancel()
