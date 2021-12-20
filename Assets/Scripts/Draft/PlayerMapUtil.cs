6a60c287c02a48afd7e76b1aa80fd87bce0cc39e
Allundo <chickenbomber@gmail.com>
2021-12-20 19:25:41 +0900

    Delete unused component class

--- Assets/Scripts/Draft/PlayerMapUtil.cs
+++
@@ -1,8 +0,0 @@
-﻿public class PlayerMapUtil : MapUtil
-{
-    public override bool IsForwardMovable => IsMovable(dir.GetForward(onTilePos), dir);
-    public override bool IsBackwardMovable => IsMovable(dir.GetBackward(onTilePos), dir.Backward);
-    public override bool IsLeftMovable => IsMovable(dir.GetLeft(onTilePos), dir.Left);
-    public override bool IsRightMovable => IsMovable(dir.GetRight(onTilePos), dir.Right);
-    public override bool IsJumpable => IsForwardLeapable && IsMovable(dir.GetForward(dir.GetForward(onTilePos)), dir);
-}
