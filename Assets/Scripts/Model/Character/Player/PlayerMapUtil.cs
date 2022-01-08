a2d936caedb26f3a0cbe7930d85c8ebd2755abb0
Allundo <chickenbomber@gmail.com>
2022-01-08 16:30:38 +0900

    Fix: enable to enter stairs also by Left, Right and Backward moving

--- Assets/Scripts/Model/Character/Player/PlayerMapUtil.cs
+++ Assets/Scripts/Model/Character/Player/PlayerMapUtil.cs
@@ -8,8 +8,8 @@ public class PlayerMapUtil : MapUtil
 
     public override void SetPosition() => SetPosition(map.InitPos);
     public override bool IsForwardMovable => IsMovable(dir.GetForward(onTilePos), dir);
-    public override bool IsBackwardMovable => IsMovable(dir.GetBackward(onTilePos), dir);
-    public override bool IsLeftMovable => IsMovable(dir.GetLeft(onTilePos), dir);
-    public override bool IsRightMovable => IsMovable(dir.GetRight(onTilePos), dir);
+    public override bool IsBackwardMovable => IsMovable(dir.GetBackward(onTilePos), dir.Backward);
+    public override bool IsLeftMovable => IsMovable(dir.GetLeft(onTilePos), dir.Left);
+    public override bool IsRightMovable => IsMovable(dir.GetRight(onTilePos), dir.Right);
     public override bool IsJumpable => IsForwardLeapable && IsMovable(GetJump, dir);
 }
