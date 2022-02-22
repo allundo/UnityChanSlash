135c90fe15910a23f0f6d11fd2ecdbe2d6304efe
Allundo <chickenbomber@gmail.com>
2022-02-23 00:01:44 +0900

    Add TargetAttack VFX and SFX to Witch

--- Assets/Scripts/View/Character/Enemy/WitchAnimFX.cs
+++ Assets/Scripts/View/Character/Enemy/WitchAnimFX.cs
@@ -1,5 +1,13 @@
 using UnityEngine;
 
-public class WitchAnimFX : EnemyAnimFX
+public class WitchAnimFX : AnimationFX
 {
+    [SerializeField] private AudioSource targetAttackSfx = null;
+    [SerializeField] protected ParticleSystem targetShockVfx = default;
+    [SerializeField] protected ParticleSystem targetTrailVfx = default;
+
+    // Called as Animation Event functions
+    public void OnAttack() => Play(targetAttackSfx, targetShockVfx);
+    public void OnTrailStart() => targetTrailVfx?.Play();
+    public void OnTrailEnd() => targetTrailVfx?.Stop(true, ParticleSystemStopBehavior.StopEmitting);
 }
