c60c99b03f12c5c28eaf8a9b537ddb8ba81967a8
Allundo <chickenbomber@gmail.com>
2022-06-27 21:02:54 +0900

    Create clone material for Graphic not to change material assets itself

--- Assets/Scripts/View/UI/FadeMaterialColor.cs
+++ Assets/Scripts/View/UI/FadeMaterialColor.cs
@@ -9,17 +9,31 @@ public class FadeMaterialColor : FadeTween
     public FadeMaterialColor(MaskableGraphic image, float maxAlpha = 1f, bool isValidOnPause = false)
         : base(image, maxAlpha, isValidOnPause)
     {
-        material = image.material;
         gameObject = image.gameObject;
+
+        if (image.material == null) Debug.LogError("GameObject " + gameObject.name + " は Material を持っていません", gameObject);
+
+        material = new Material(image.material);
+        image.material = material;
     }
 
     public FadeMaterialColor(GameObject gameObject, float maxAlpha = 1f, bool isValidOnPause = false)
         : base(gameObject.GetComponent<MaskableGraphic>(), maxAlpha, isValidOnPause)
     {
-        material = gameObject.GetComponent<MaskableGraphic>()?.material
-            ?? gameObject.GetComponent<Renderer>()?.material;
-
         this.gameObject = gameObject;
+
+        // GameObject is MaskableGraphic
+        if (image != null)
+        {
+            material = new Material(image.material);
+            image.material = material;
+        }
+        // GameObject has Renderer
+        else
+        {
+            material = gameObject.GetComponent<Renderer>()?.material;
+            if (material == null) Debug.LogError("GameObject " + gameObject.name + " は Material を持っていません", gameObject);
+        }
     }
 
     public override Color color
@@ -43,5 +57,24 @@ public class FadeMaterialColor : FadeTween
     {
         CompleteTweens(); // Complete fade tweens before switching material
         this.material = material;
+    }
+
+    protected class GraphicMaterial
+    {
+        private Material copiedMaterial;
+        private MaskableGraphic graphic;
+
+        public GraphicMaterial(MaskableGraphic graphic)
+        {
+            this.graphic = graphic;
+            copiedMaterial = graphic.materialForRendering;
+            graphic.material = copiedMaterial;
+        }
+    }
+
+    public override void OnDestroy()
+    {
+        // Destroy the cloned material
+        Object.Destroy(material);
     }
 }
