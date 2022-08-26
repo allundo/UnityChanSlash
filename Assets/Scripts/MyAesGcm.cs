63ee148c965ab850b0b73e80716d8a8f7e07dcdd
Allundo <chickenbomber@gmail.com>
2022-08-26 16:22:54 +0900

    Fix: apply SHA256 hash

--- Assets/Scripts/MyAesGcm.cs
+++ Assets/Scripts/MyAesGcm.cs
@@ -3,10 +3,14 @@ using System.IO.Compression;
 using System.Security.Cryptography;
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 public class MyAesGcm
 {
     private byte[] key;
+    private SHA256Hash hash = new SHA256Hash();
+    private NonceStore nonceStore;
+
     private RNGCryptoServiceProvider rngCSP = new RNGCryptoServiceProvider();
     private byte[] GetBytes(int size)
     {
@@ -15,14 +19,13 @@ public class MyAesGcm
         return ret;
     }
 
-    private Dictionary<byte[], byte[]> nonceStore = new Dictionary<byte[], byte[]>();
-
-    public MyAesGcm(byte[] key)
+    public MyAesGcm(byte[] key, byte[] tagHashKey = null)
     {
         this.key = key;
+        nonceStore = new NonceStore(tagHashKey ?? key);
     }
 
-    public KeyValuePair<byte[], string> Encrypt(string plainText)
+    public KeyValuePair<string, string> Encrypt(string plainText)
     {
         using (Aes aesAlg = Aes.Create())
         {
@@ -31,7 +34,7 @@ public class MyAesGcm
             aesAlg.Key = key;
             aesAlg.IV = nonce;
 
-            var tag = SHA256Hash(Convert.ToBase64String(nonce) + plainText);
+            var tag = hash.String(plainText, nonce);
             nonceStore[tag] = nonce;
 
             // Create an encryptor to perform the stream transform.
@@ -50,14 +53,14 @@ public class MyAesGcm
                             swEncrypt.Write(plainText);
                         }
 
-                        return new KeyValuePair<byte[], string>(tag, Convert.ToBase64String(msEncrypt.ToArray()));
+                        return new KeyValuePair<string, string>(tag, Convert.ToBase64String(msEncrypt.ToArray()));
                     }
                 }
             }
         }
     }
 
-    public string Decrypt(string cipherText, byte[] tag)
+    public string Decrypt(string cipherText, string tag)
     {
         using (Aes aesAlg = Aes.Create())
         {
@@ -78,9 +81,9 @@ public class MyAesGcm
                         {
                             var plainText = srDecrypt.ReadToEnd();
 
-                            if (!Convert.ToBase64String(tag).Equals(Convert.ToBase64String(SHA256Hash(Convert.ToBase64String(nonceStore[tag]) + plainText))))
+                            if (!hash.Check(tag, plainText, aesAlg.IV))
                             {
-                                throw new CryptographicException("復号失敗");
+                                throw new CryptographicException("復号失敗: 改ざん疑惑");
                             }
 
                             return plainText;
@@ -91,18 +94,81 @@ public class MyAesGcm
         }
     }
 
-    private byte[] SHA256Hash(string text)
+    private class NonceStore
     {
-        using (MemoryStream msSHA256 = new MemoryStream())
+        private byte[] key;
+        private Dictionary<string, byte[]> nonceStore;
+        private SHA256Hash hash = new SHA256Hash();
+
+        public NonceStore(byte[] key, Dictionary<string, byte[]> nonceStore = null)
         {
-            using (SHA256 mySHA256 = SHA256.Create())
+            this.key = key;
+            this.nonceStore = nonceStore ?? new Dictionary<string, byte[]>();
+        }
+
+        public byte[] this[string tag]
+        {
+            get
             {
-                using (StreamWriter swRaw = new StreamWriter(msSHA256))
+                byte[] nonce;
+                if (!nonceStore.TryGetValue(hash.String(tag, key), out nonce))
                 {
-                    swRaw.Write(text);
+                    throw new CryptographicException("復号失敗: 不正タグ");
+                }
+                return nonce;
+            }
+            set
+            {
+                nonceStore[hash.String(tag, key)] = value;
+            }
+        }
+    }
+
+    public class SHA256Hash
+    {
+        public bool Check(byte[] hash, byte[] raw, byte[] key)
+            => Convert.ToBase64String(hash).Equals(String(raw, key));
+
+        public bool Check(string hash, string raw, byte[] key)
+            => hash.Equals(String(raw, key));
+
+        public byte[] Bytes(byte[] src)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                using (MemoryStream msSHA256 = new MemoryStream(src))
+                {
+                    return sha256.ComputeHash(msSHA256);
                 }
             }
-            return msSHA256.ToArray();
         }
+
+        public byte[] Bytes(byte[] src, byte[] key) => Bytes(key.Concat(src).ToArray());
+
+        public byte[] Bytes(string text)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                using (MemoryStream msSHA256 = new MemoryStream())
+                {
+                    using (StreamWriter swRaw = new StreamWriter(msSHA256))
+                    {
+                        swRaw.Write(text);
+
+                        msSHA256.Position = 0;
+                        return sha256.ComputeHash(msSHA256);
+                    }
+                }
+            }
+        }
+
+        public byte[] Bytes(string text, byte[] key) => Bytes(Convert.ToBase64String(key) + text);
+
+        public string String(byte[] src) => Convert.ToBase64String(Bytes(src));
+        public string String(byte[] src, byte[] key) => String(key.Concat(src).ToArray());
+
+        public string String(string text) => Convert.ToBase64String(Bytes(text));
+        public string String(string text, byte[] key) => String(Convert.ToBase64String(key) + text);
+
     }
 }
