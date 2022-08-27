3cc61981f4f2bd791bdca08612c66478df785c73
Allundo <chickenbomber@gmail.com>
2022-08-27 17:42:47 +0900

    Fix: fix meaningless hash methods

--- Assets/Scripts/Presenter/DataIO/SHA256Hash.cs
+++ Assets/Scripts/Presenter/DataIO/SHA256Hash.cs
@@ -1,52 +1,30 @@
 using System;
+using System.Text;
 using System.Linq;
-using System.IO;
 using System.Security.Cryptography;
 
 public class SHA256Hash
 {
+    private static readonly SHA256 sha256 = SHA256.Create();
+
     public bool Check(byte[] hash, byte[] raw, byte[] key)
         => Convert.ToBase64String(hash).Equals(String(raw, key));
 
     public bool Check(string hash, string raw, byte[] key)
         => hash.Equals(String(raw, key));
 
-    public byte[] Bytes(byte[] src)
-    {
-        using (SHA256 sha256 = SHA256.Create())
-        {
-            using (MemoryStream msSHA256 = new MemoryStream(src))
-            {
-                return sha256.ComputeHash(msSHA256);
-            }
-        }
-    }
+    public byte[] Bytes(byte[] src) => sha256.ComputeHash(src);
 
     public byte[] Bytes(byte[] src, byte[] key) => Bytes(key.Concat(src).ToArray());
 
     public byte[] Bytes(string text)
-    {
-        using (SHA256 sha256 = SHA256.Create())
-        {
-            using (MemoryStream msSHA256 = new MemoryStream())
-            {
-                using (StreamWriter swRaw = new StreamWriter(msSHA256))
-                {
-                    swRaw.Write(text);
+         => sha256.ComputeHash(Encoding.UTF8.GetBytes(text));
 
-                    msSHA256.Position = 0;
-                    return sha256.ComputeHash(msSHA256);
-                }
-            }
-        }
-    }
-
-    public byte[] Bytes(string text, byte[] key) => Bytes(Convert.ToBase64String(key) + text);
+    public byte[] Bytes(string text, byte[] key) => Bytes(key.Concat(Encoding.UTF8.GetBytes(text)).ToArray());
 
     public string String(byte[] src) => Convert.ToBase64String(Bytes(src));
     public string String(byte[] src, byte[] key) => String(key.Concat(src).ToArray());
 
     public string String(string text) => Convert.ToBase64String(Bytes(text));
-    public string String(string text, byte[] key) => String(Convert.ToBase64String(key) + text);
-
+    public string String(string text, byte[] key) => String(key.Concat(Encoding.UTF8.GetBytes(text)).ToArray());
 }
