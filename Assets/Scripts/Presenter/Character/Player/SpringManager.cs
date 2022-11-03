5a733529368abac52e8635c70898f76968f717b5
Allundo <chickenbomber@gmail.com>
2022-11-03 20:32:50 +0900

    Handle UnityChan SpringBone inside game module

---
+++ Assets/Scripts/Presenter/Character/Player/SpringManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System;
+
+public class SpringManager : MonoBehaviour
+{
+    protected static readonly int FRAME_RATE = 60;
+    protected static readonly float FRAME_SEC_UNIT = 1f / (float)FRAME_RATE;
+
+    public SpringBone[] springBones;
+
+    private float DeltaTime => Time.deltaTime > 0f ? Time.deltaTime : 1f / FPS;
+    private int FPS => Application.targetFrameRate > 0 ? Application.targetFrameRate : 60;
+
+    void LateUpdate()
+    {
+        Array.ForEach(springBones, bone => bone.UpdateSpring(DeltaTime));
+    }
+}
---
+++ Assets/Scripts/View/Character/Player/SpringCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SpringCollider : MonoBehaviour
+{
+    //半径
+    public float radius = 0.5f;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+}
