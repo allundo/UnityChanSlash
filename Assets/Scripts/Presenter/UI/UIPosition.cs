b5df0e48f4a8cb7e0c9a834e24b9576cd39916b3
Allundo <chickenbomber@gmail.com>
2023-02-09 17:42:53 +0900

    Support device rotation for RankInMessage on GameOver UI

--- Assets/Scripts/Presenter/UI/UIPosition.cs
+++ Assets/Scripts/Presenter/UI/UIPosition.cs
@@ -10,6 +10,7 @@ public class UIPosition : MonoBehaviour
     [SerializeField] private RestButton restButton = default;
     [SerializeField] private ResumeButton resumeButton = default;
     [SerializeField] private ActiveMessageController activeMessageUI = default;
+    [SerializeField] private RankInMessage rankInMessage = default;
     [SerializeField] private float portraitFromBottom = 720f;
     [SerializeField] private float landscapeFromLeft = 480f;
     [SerializeField] private float lifeGaugeFromBottom = 60f;
@@ -40,6 +41,7 @@ public class UIPosition : MonoBehaviour
         restButton.ResetOrientation(orientation);
         resumeButton.ResetOrientation(orientation);
         activeMessageUI.ResetOrientation(orientation);
+        rankInMessage.ResetOrientation(orientation);
         ItemInventory.Instance.ResetOrientation(orientation);
         fightCircle.ResetCenterPos();
     }
--- Assets/Scripts/View/UI/GameOver/RankInMessage.cs
+++ Assets/Scripts/View/UI/GameOver/RankInMessage.cs
@@ -7,26 +7,37 @@ public class RankInMessage : MonoBehaviour
     [SerializeField] private ParticleSystem shootEffect = default;
     private UITween uiTween;
     private FadeTween bgFade;
+    private RectTransform rectTransform;
+
+    private float moveX;
 
     void Awake()
     {
         uiTween = new UITween(rankInText);
         uiTween.SetPosX(Screen.width);
+        rectTransform = GetComponent<RectTransform>();
         bgFade = new FadeTween(gameObject, 0.5f);
         bgFade.SetAlpha(0f);
+    }
+
+    public void ResetOrientation(DeviceOrientation orientation)
+    {
+        rectTransform.sizeDelta = new Vector2(Screen.width, rectTransform.sizeDelta.y);
+        moveX = -(Screen.width + uiTween.defaultSize.x) * 0.5f;
+        uiTween.SetPos(new Vector2(-moveX, 0f), true);
     }
 
     public Tween RankInTween()
     {
         return DOTween.Sequence()
-            .AppendCallback(() => uiTween.SetPosX(Screen.width))
+            .AppendCallback(() => uiTween.ResetPos())
             .AppendCallback(() => textEffect.StopAndClear())
             .AppendCallback(() => textEffect.PlayEx())
             .AppendCallback(() => shootEffect.PlayEx())
-            .Append(uiTween.MoveX(-Screen.width, 1.5f).SetEase(Ease.OutExpo))
+            .Append(uiTween.MoveX(moveX, 1.5f).SetEase(Ease.OutExpo))
             .Join(bgFade.In(1.5f, 0, null, null, false))
             .AppendInterval(0.25f)
-            .Append(uiTween.MoveX(-Screen.width, 1f).SetEase(Ease.InExpo))
+            .Append(uiTween.MoveX(moveX, 1f).SetEase(Ease.InExpo))
             .Join(bgFade.Out(1f, 0, null, null, false).SetEase(Ease.InQuad));
     }
 }
