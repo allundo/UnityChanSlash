d95d080f80346a2da55b43fb5515501b07d571b7
Allundo <chickenbomber@gmail.com>
2023-06-23 18:01:30 +0900

    Refine room creating method

--- Assets/Scripts/Model/Map/MapManager.cs
+++ Assets/Scripts/Model/Map/MapManager.cs
@@ -85,9 +85,7 @@ public class MapManager
 
         dirMap = new Dir[width, height];
 
-        var maze = new MazeCreator(width, height, new Random())
-            .CreateMaze()
-            .CreateRooms();
+        var maze = new MazeCreator(width, height, new Random());
 
         matrix = maze.Matrix.Clone() as Terrain[,];
         deadEndPos = new Dictionary<Pos, IDirection>(maze.SearchDeadEnds());
@@ -607,6 +605,9 @@ public class MapManager
         return false;
     }
 
+    /// <summary>
+    /// matrix[,] must be set before call this.
+    /// </summary>
     private void CreateDirMap()
     {
         // Set direction to door and wall
@@ -737,10 +738,33 @@ public class MapManager
         private readonly int WALL = (int)Terrain.Wall;
         private readonly int DOOR = (int)Terrain.Door;
 
-        public int[,] matrix { get; private set; }
+        private int[,] matrix;
+        public int[] CopyMapData()
+        {
+            int[] ret = new int[Width * Height];
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    ret[x + y * Width] = matrix[x, y];
+                }
+            }
+            return ret;
+        }
 
-        // 2次元配列の迷路情報
         public Terrain[,] Matrix { get; protected set; }
+
+        private void SetTerrainData(int[,] matrix)
+        {
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    Matrix[x, y] = Util.ConvertTo<Terrain>(matrix[x, y]);
+                }
+            }
+        }
+
         public int Width { get; private set; }
         public int Height { get; private set; }
 
@@ -755,26 +779,31 @@ public class MapManager
             // Minimum size is 5 and even-number size is not available.
             if (width < 5 || height < 5 || width % 2 == 0 || height % 2 == 0) throw new ArgumentOutOfRangeException();
 
-            // 迷路情報を初期化
             this.Width = width;
             this.Height = height;
-            this.matrix = new int[width, height];
-            this.Matrix = new Terrain[width, height];
 
+            this.Matrix = new Terrain[width, height];
             this.rnd = rnd ?? new Random();
+
+            var matrix = CreateMaze(width, height);
+            this.roomCenterPos = CreateRooms(matrix, width, height);
+            SetTerrainData(matrix);
+
+            this.matrix = matrix;
         }
 
-        public MazeCreator CreateMaze()
+        private int[,] CreateMaze(int width, int height)
         {
             List<Pos> startCells = new List<Pos>();
+            int[,] matrix = new int[width, height];
 
             // Set WALLs to edges of maze.
-            for (int y = 0; y < Height; y++) matrix[0, y] = matrix[Width - 1, y] = WALL;
-            for (int x = 0; x < Width; x++) matrix[x, 0] = matrix[x, Height - 1] = WALL;
+            for (int y = 0; y < height; y++) matrix[0, y] = matrix[width - 1, y] = WALL;
+            for (int x = 0; x < width; x++) matrix[x, 0] = matrix[x, height - 1] = WALL;
 
-            for (int y = 1; y < Height - 1; y++)
+            for (int y = 1; y < height - 1; y++)
             {
-                for (int x = 1; x < Width - 1; x++)
+                for (int x = 1; x < width - 1; x++)
                 {
                     // Initialize inside edges of maze by PATHs.
                     matrix[x, y] = PATH;
@@ -792,22 +821,15 @@ public class MapManager
             while (startCells.Count > 0)
             {
                 Stack<Pos> usedGrids = new Stack<Pos>();
-                ExtendWall(startCells[0], usedGrids);
+                ExtendWall(matrix, startCells[0], usedGrids);
 
                 startCells.Remove(usedGrids);
             }
 
-            for (int y = 0; y < Height; y++)
-            {
-                for (int x = 0; x < Width; x++)
-                {
-                    Matrix[x, y] = Util.ConvertTo<Terrain>(matrix[x, y]);
-                }
-            }
-            return this;
+            return matrix;
         }
 
-        private bool SetWall(Pos pos, Pos dir, Stack<Pos> currentGrids)
+        private bool TrySetWall(int[,] matrix, Pos pos, Pos dir, Stack<Pos> currentGrids)
         {
             var wallPos = pos + dir;
             var destPos = wallPos + dir;
@@ -819,7 +841,7 @@ public class MapManager
             return true;
         }
 
-        private bool ExtendWall(Pos pos, Stack<Pos> currentGrids)
+        private bool ExtendWall(int[,] matrix, Pos pos, Stack<Pos> currentGrids)
         {
             currentGrids.Push(pos);
 
@@ -832,10 +854,10 @@ public class MapManager
 
             foreach (Pos dir in dirCandidates)
             {
-                if (SetWall(pos, dir, currentGrids))
+                if (TrySetWall(matrix, pos, dir, currentGrids))
                 {
                     // Achieve to wall and stop extending if false.
-                    if (!ExtendWall(pos + dir * 2, currentGrids)) return false;
+                    if (!ExtendWall(matrix, pos + dir * 2, currentGrids)) return false;
                 }
             }
 
@@ -876,40 +898,35 @@ public class MapManager
             return deadEndPos;
         }
 
-        public MazeCreator CreateRooms()
+        private List<Pos> CreateRooms(int[,] matrix, int width, int height)
         {
-            int wHalf = (Width + 1) / 2;
-            int hHalf = (Height + 1) / 2;
+            int wHalf = (width + 1) / 2;
+            int hHalf = (height + 1) / 2;
 
             int numOfDivide = (wHalf + hHalf) / 16;
-            int numOfRandom = (wHalf + hHalf) / 10;
 
-            Console.WriteLine("wHalf: " + wHalf + ", hHalf: " + hHalf + ", numOfDivide: " + numOfDivide + ", numOfRandom: " + numOfRandom);
+            List<Pos> roomCenterPos = new List<Pos>();
 
+            // Get HALF of the room left-top points.
             List<Pos> dividingRandomPos = GetDividingRandomPos(wHalf, hHalf, new Pos(0, 0), numOfDivide, 1);
 
             foreach (Pos pos in dividingRandomPos)
             {
-                MakeRoom(pos);
+                roomCenterPos.Add(MakeRoom(matrix, width, height, pos));
             }
 
-            for (int i = 0; i < numOfRandom; i++)
-            {
-                // MakeRoom(GetRandomPos(wHalf, hHalf, new Pos(0, 0), 1));
-            }
-
-            return this;
+            return roomCenterPos;
         }
 
         /// <summary>
-        /// 特定の広さの領域を4分割して、それぞれの分割領域からランダム座標を取得
+        /// 特定の広さの領域を4分割して、それぞれの分割領域からランダム座標を取得 <br />
         /// numOfDivide で分割回数を指定し、再帰的に分割を行った領域から座標を取得する
         /// </summary>
-        /// <param name="w"></param>
-        /// <param name="h"></param>
-        /// <param name="offset"></param>
-        /// <param name="numOfDivide"></param>
-        /// <param name="trim"></param>
+        /// <param name="w">width of dividing region</param>
+        /// <param name="h">height of dividing region</param>
+        /// <param name="offset">left top of dividing region</param>
+        /// <param name="numOfDivide">repeat times of dividing</param>
+        /// <param name="trim">shrink size of dividing region</param>
         /// <returns></returns>
         private List<Pos> GetDividingRandomPos(int w, int h, Pos offset, int numOfDivide = 1, int trim = 0)
         {
@@ -926,51 +943,54 @@ public class MapManager
             .Concat(GetDividingRandomPos(w / 2, h / 2, offset.AddY(h / 2), numOfDivide - 1))
             .Concat(GetDividingRandomPos(w / 2, h / 2, offset.AddX(w / 2).AddY(h / 2), numOfDivide - 1)).ToList<Pos>();
         }
-        private Pos GetRandomPos(int w, int h, Pos offset, int trim = 0)
-            => new Pos(offset.x + rnd.Next(0, w - trim), offset.y + rnd.Next(0, h - trim));
+        private Pos GetRandomPos(int w, int h, Pos offset)
+            => new Pos(offset.x + rnd.Next(0, w), offset.y + rnd.Next(0, h));
 
-        private void MakeRoom(Pos posHalf)
+        /// <summary>
+        /// Create a room into maze data matrix.
+        /// </summary>
+        /// <param name="matrix">Maze data matrix to edit</param>
+        /// <param name="width">Width of the maze</param>
+        /// <param name="height">Height of the maze</param>
+        /// <param name="posHalf">HALF of the room left-top point.</param>
+        /// <returns>Room center position</returns>
+        private Pos MakeRoom(int[,] matrix, int width, int height, Pos posHalf)
         {
+            // pos.x, pos.y, w, h must be even-number to adjust rooms to grid points.
+            Pos pos = posHalf * 2;
+
             int w = rnd.Next(2, 4) * 2;
             int h = rnd.Next(2, 4) * 2;
 
-            Pos pos = posHalf * 2;
-
             // Shrinks room size if exceeds region
             // Extends room size 1 block if touches outer wall
-            if (pos.x + w > Width - 1) w = Width - 1 - pos.x;
-            if (pos.y + h > Height - 1) h = Height - 1 - pos.y;
-
-            roomCenterPos.Add(pos + new Pos(w / 2, h / 2));
+            if (pos.x + w > width - 1) w = width - 1 - pos.x;
+            if (pos.y + h > height - 1) h = height - 1 - pos.y;
 
             // Set inner surface as ground
             for (int i = pos.x + 1; i < pos.x + w; i++)
             {
                 for (int j = pos.y + 1; j < pos.y + h; j++)
                 {
-                    Matrix[i, j] = Terrain.Ground;
+                    matrix[i, j] = GROUND;
                 }
             }
 
             // Set door as path on room edge
             for (int i = pos.x + 1; i < pos.x + w; i++)
             {
-                SetDoor(i, pos.y);
-                SetDoor(i, pos.y + h);
+                if (matrix[i, pos.y] == PATH) matrix[i, pos.y] = DOOR;
+                if (matrix[i, pos.y + h] == PATH) matrix[i, pos.y + h] = DOOR;
             }
+
             for (int j = pos.y + 1; j < pos.y + h; j++)
             {
-                SetDoor(pos.x, j);
-                SetDoor(pos.x + w, j);
+                if (matrix[pos.x, j] == PATH) matrix[pos.x, j] = DOOR;
+                if (matrix[pos.x + w, j] == PATH) matrix[pos.x + w, j] = DOOR;
             }
-        }
 
-        private void SetDoor(int x, int y)
-        {
-            if (Matrix[x, y] == Terrain.Path)
-            {
-                Matrix[x, y] = Terrain.Door;
-            }
+            // Room center position
+            return pos + new Pos(w / 2, h / 2);
         }
     }
 }
