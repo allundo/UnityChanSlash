a0ecf11180c28051b37077f7094f07dd853478f9
Allundo <chickenbomber@gmail.com>
2023-11-02 04:10:27 +0900

    Apply VFX and SFX to Anna slash animation

--- Assets/Scripts/View/Character/Enemy/AnnaAnimFX.cs
+++ Assets/Scripts/View/Character/Enemy/AnnaAnimFX.cs
@@ -4,14 +4,38 @@ public class AnnaAnimFX : ShieldAnimFX
 {
     [SerializeField] protected ParticleSystem auraVfx = default;
     [SerializeField] protected ParticleSystem fireAuraVfx = default;
+    [SerializeField] protected ParticleSystem slashEnchantVfx = default;
+    [SerializeField] protected AudioSource slashEnchantSnd = default;
+    [SerializeField] protected ParticleSystem slashTornadoVfx = default;
+    [SerializeField] protected AudioSource slashTornadoSnd = default;
+    [SerializeField] protected ParticleSystem slashEndVfx = default;
 
     public void OnFire()
     {
         fireAuraVfx.PlayEx();
     }
 
+    public void OnCrouching()
+    {
+        slashEnchantVfx.PlayEx();
+        slashEnchantSnd.PlayEx();
+    }
+
+    public void OnSlash()
+    {
+        slashTornadoVfx.PlayEx();
+        slashTornadoSnd.PlayEx();
+    }
+
+    public void OnSlashEnd()
+    {
+        slashEndVfx.PlayEx();
+    }
+
     public override void StopVFX()
     {
+        slashEnchantVfx.StopEmitting();
+        slashTornadoVfx.StopEmitting();
         auraVfx.StopEmitting();
         shieldVfx.StopEmitting();
     }
